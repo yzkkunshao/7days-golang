476cfb337e9fad4fbc2c67f776aff83633d1528b
yzkkunshao <55026519+yzkkunshao@users.noreply.github.com>
2024-11-24 20:32:40 +0800

    gin

---
+++ quick-gin/main.go
@@ -0,0 +1,30 @@
+// geektutu.com
+// main.go
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func main() {
+	stu1 := &student{"Tom", 18}
+	stu2 := &student{"Picky", 18}
+
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/arr", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gin.H{
+			"title":  "Gin",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+
+	r.Run(":9999") // listen and serve on 0.0.0.0:8080
+}
